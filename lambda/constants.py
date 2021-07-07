3bf82e4d48a9a66616f85d7fe3be9b6ba3de271f
rishim1402 <rishim1402@gmail.com>
2021-07-07 18:46:17 +0530

    feat: Implemented Data retreival functionality

--- lambda/constants.py
+++ lambda/constants.py
@@ -2,3 +2,14 @@ NOTION_VERSION="2021-05-13"
 
 NOTION_SEARCH_URL="https://api.notion.com/v1/search"
 NOTION_RETRIEVE_BLOCK_CHILDREN_URL="https://api.notion.com/v1/blocks/<blockId>/children"
+
+TYPE = {
+    "todo" : "to_do",
+    "paragraph" : "paragraph",
+    "heading" : "heading_1",
+    "subheading" : "heading_2",
+    "bulleted list" : "bulleted_list_item",
+    "numbered list" : "numbered_list_item",
+    "toggle" : "toggle",
+    "child page" : "child_page",
+}
--- lambda/notion/helpers.py
+++ lambda/notion/helpers.py
@@ -1,12 +1,33 @@
+from lambda.constants import TYPE
 import logging
+from typing import List
+import os
+import sys
+sys.path.append("./")
+import constants
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
+
+
 
 def isTypeWorkspace(page):
     if(page.get("parent") and page.get("parent").get("type") == "workspace"):
         return True
     else:
         return False
+
+def isType(block,type):
+    if(block.get("type") == constants.TYPE[type]):
+        return True
+    else:
+        return False
+
+def getPlainText(block):
+    try:
+        return block.get(block.get("type")).get("text").get("plain_text")
+    except:
+        return "text not found!"
+
 
 def getPageName(page):
     try:
@@ -16,3 +37,11 @@ def getPageName(page):
             return page.get("title")[0].get("text").get("content")
     except :
         logger.error("Page not available")
+
+def getDataFromBlockBasedType(type,blocks):
+    try:
+        typeObjects = list(map(getPlainText, filter(lambda block: isType(block,type), blocks)))
+        return typeObjects
+    except:
+        logger.error("block not found")
+        return list("Data could not be extracted")
